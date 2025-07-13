697562317f33ca26f41a9e81163716ba2ef8a11c
xrip <xrip@xrip.ru>
2025-07-13 12:27:15 +0300

    Fix win32 build

--- src/emulator/emulator.h
+++ src/emulator/emulator.h
@@ -171,7 +171,12 @@ uint16_t cga_portin(uint16_t portnum);
 extern uint32_t vga_plane_offset;
 extern uint8_t vga_planar_mode;
 
-extern bool ega_vga_enabled;
+#if PICO_ON_DEVICE
+    extern bool ega_vga_enabled;
+#else
+#define ega_vga_enabled (1)
+#endif
+
 void vga_portout(uint16_t portnum, uint16_t value);
 
 uint16_t vga_portin(uint16_t portnum);
